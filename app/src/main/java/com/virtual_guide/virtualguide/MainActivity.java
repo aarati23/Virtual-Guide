155a4d8cc92116cbe489b0e946ad7628a11ba5fa
Saurabh <saurabhthakre165@gmail.com>
2020-01-23 09:33:35 +0530

    Initial commit

---
+++ app/src/main/java/com/virtual_guide/virtualguide/MainActivity.java
@@ -0,0 +1,14 @@
+package com.virtual_guide.virtualguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
